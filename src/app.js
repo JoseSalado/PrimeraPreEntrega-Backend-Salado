22f6e5965de31ea6986b9679b1bbcb8b62d623b1
JoseSalado <saladojosemiguel@gmail.com>
2023-01-19 10:17:35 -0300

    second commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const productManager = require('../src/ProductManager.js');
+const express = require('express')
+const routes = require('./routes/index')
+const port = 8080
+const app = express();
+
+// const pm1 = new productManager("./dataBase.json");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+routes(app)
+
+app.listen(port, () => console.log(`Servidor en el puerto ${port}`));
